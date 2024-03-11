7837ea14fbdd2724f83948602db5487ee612cf3b
Nicolas <nicolas.echenique.117@gmail.com>
2024-03-11 15:54:40 -0300

    init commit

---
+++ src/test/java/com/registro/grupos/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registro.grupos.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
