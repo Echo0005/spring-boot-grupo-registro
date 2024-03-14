75da87316b172b1c89b96e08c85ba8231e5dbe5d
Nicolas <nicolas.echenique.117@gmail.com>
2024-03-14 14:46:21 -0300

    Agregando Metodos a los Servicios

--- src/main/java/com/registro/grupos/demo/service/GroupService.java
+++ src/main/java/com/registro/grupos/demo/service/GroupService.java
@@ -1,8 +1,11 @@
 package com.registro.grupos.demo.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.registro.grupos.demo.model.Group;
 import com.registro.grupos.demo.repository.IGroupRepository;
 
 import jakarta.transaction.Transactional;
@@ -14,5 +17,29 @@ public class GroupService
     @Autowired
     private IGroupRepository iGroupRepository;
 
+    public Group findOne (Long id)
+    {
+        return iGroupRepository.findById(id).orElseThrow();
+    }
+
+    public List<Group> findAll()
+    {
+        return iGroupRepository.findAll();
+    }
+
+    public Group addGroup ( Group group )
+    {
+        return iGroupRepository.save(group);
+    }
+
+    public Group editGroup ( Group group )
+    {
+        return iGroupRepository.save(group);
+    }
+
+    public void deleteGroup (Long id)
+    {
+        iGroupRepository.deleteById(id);
+    }
     
 }
