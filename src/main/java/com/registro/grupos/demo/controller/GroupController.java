cdb9fdf14526afcfe4e5dd2ca0b69891d86582c8
Nicolas <nicolas.echenique.117@gmail.com>
2024-03-27 09:57:15 -0300

    Agregando controller de Group y User

--- src/main/java/com/registro/grupos/demo/controller/GroupController.java
+++ src/main/java/com/registro/grupos/demo/controller/GroupController.java
@@ -1,10 +1,23 @@
 package com.registro.grupos.demo.controller;
 
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.registro.grupos.demo.service.GroupService;
+import com.registro.grupos.demo.dto.GroupDTO;
+import com.registro.grupos.demo.dto.Message;
+import com.registro.grupos.demo.model.Group;
 
 @RestController
 @RequestMapping("/group")
@@ -12,5 +25,57 @@ public class GroupController
 {
     @Autowired
     private GroupService groupService;
+
+    
+    @GetMapping("/all")
+    public ResponseEntity<List<Group>> findAll()
+    {
+        List<Group> listGroup = groupService.findAll();
+        return new ResponseEntity<>(listGroup, HttpStatus.FOUND);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addGroup( @RequestBody GroupDTO groupDTO )
+    {
+        if ( StringUtils.isBlank( groupDTO.getLogo() ) )
+            return new ResponseEntity<>( new Message("Logo vacio"), HttpStatus.LENGTH_REQUIRED );
+        if ( StringUtils.isBlank( groupDTO.getName() ) )
+            return new ResponseEntity<>( new Message("Nombre vacio"), HttpStatus.LENGTH_REQUIRED );
+        
+        Group newGroup = new Group();
+        newGroup.setLogo( groupDTO.getLogo() );
+        newGroup.setName( groupDTO.getName() );
+
+        groupService.addGroup(newGroup);
+
+        return new ResponseEntity<>( new Message("Grupo creado"), HttpStatus.CREATED );
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<?> updateGroup( @RequestBody GroupDTO groupDTO )
+    {
+        if ( groupService.existById( groupDTO.getId() ) )
+            return new ResponseEntity<>( new Message("Id no encontrado"), HttpStatus.NOT_MODIFIED );
+        
+        Group group = new Group();
+        group.setId( groupDTO.getId() );
+        group.setLogo( groupDTO.getLogo() );
+        group.setName( groupDTO.getName() );
+
+        groupService.addGroup( group );
+
+        return new ResponseEntity<>( new Message( "Grupo actualizado" ), HttpStatus.OK );
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> deleteGroup( @RequestBody GroupDTO groupDTO )
+    {
+        if ( groupService.existById( groupDTO.getId() ) )
+            return new ResponseEntity<>( new Message("Grupo no encontrado"), HttpStatus.NOT_FOUND );
+        
+        groupService.deleteGroup( groupDTO.getId() );
+
+        return new ResponseEntity<>( new Message("Grupo eliminado"), HttpStatus.OK );
+    }
 
 }
--- src/main/java/com/registro/grupos/demo/controller/UserController.java
+++ src/main/java/com/registro/grupos/demo/controller/UserController.java
@@ -1,9 +1,22 @@
 package com.registro.grupos.demo.controller;
 
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.registro.grupos.demo.dto.Message;
+import com.registro.grupos.demo.dto.UserDTO;
+import com.registro.grupos.demo.model.User;
 import com.registro.grupos.demo.service.UserService;
 
 @RestController
@@ -12,4 +25,55 @@ public class UserController
 {
     @Autowired
     private UserService userService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<User>> findAll()
+    {
+        List<User> listUser = userService.findAll();
+        return new ResponseEntity<>( listUser, HttpStatus.FOUND );
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity<?> addUser( @RequestBody UserDTO userDTO )
+    {
+        if ( StringUtils.isBlank( userDTO.getName ) )
+            return new ResponseEntity<>( new Message("Nombre vacio"), HttpStatus.LENGTH_REQUIRED );
+        if ( StringUtils.isBlank( userDTO.getAvatar() ) )
+            return new ResponseEntity<>( new Message("Avatar vacio"), HttpStatus.LENGTH_REQUIRED );
+        
+        User newUser = new User();
+        newUser.setName( userDTO.getName() );
+        newUser.setAvatar( userDTO.getAvatar() );
+
+        userService.addUser(newUser);
+
+        return new ResponseEntity<>( new Message("Usuario creado"), HttpStatus.CREATED );
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<?> updateUser( @RequestBody UserDTO userDTO )
+    {
+        if ( userService.existById( userDTO.getId() ) )
+            return new ResponseEntity<>( new Message("Id no encontrado"), HttpStatus.NOT_MODIFIED );
+        
+        User updateUser = new User();
+        updateUser.setId( userDTO.getId() );
+        updateUser.setLogo( userDTO.getName() );
+        updateUser.setName( userDTO.getAvatar() );
+
+        userService.addUser( updateUser );
+
+        return new ResponseEntity<>( new Message( "Usuario actualizado" ), HttpStatus.OK );
+    }
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<?> deleteUser( @RequestBody UserDTO userDTO )
+    {
+        if ( userService.existById( userDTO.getId() ) )
+            return new ResponseEntity<>( new Message("Usuario no encontrado"), HttpStatus.NOT_FOUND );
+        
+        userService.deleteUser( userDTO.getId() );
+
+        return new ResponseEntity<>( new Message("Usuario eliminado"), HttpStatus.OK );
+    }
 }
