78b0bbccdc2c3965dee6acdeaeb05126e66b33d7
Nicolas <nicolas.echenique.117@gmail.com>
2024-04-12 18:06:19 -0300

    Agregando query sql para busquedas con multiples ids

--- src/main/java/com/registro/grupos/demo/repository/IGrupoRepository.java
+++ src/main/java/com/registro/grupos/demo/repository/IGrupoRepository.java
@@ -1,6 +1,9 @@
 package com.registro.grupos.demo.repository;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.registro.grupos.demo.model.Grupo;
@@ -8,5 +11,10 @@ import com.registro.grupos.demo.model.Grupo;
 @Repository
 public interface IGrupoRepository extends JpaRepository< Grupo, Long >
 {
-
+    @Query
+    (
+        value = "SELECT g.* FROM grupo g WHERE g.id IN(?1)",
+        nativeQuery = true
+    )
+    Set<Grupo> searchGruposById( Set<Long> ids );
 }
--- src/main/java/com/registro/grupos/demo/repository/IUserRepository.java
+++ src/main/java/com/registro/grupos/demo/repository/IUserRepository.java
@@ -1,6 +1,9 @@
 package com.registro.grupos.demo.repository;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.registro.grupos.demo.model.User;
@@ -8,5 +11,10 @@ import com.registro.grupos.demo.model.User;
 @Repository
 public interface IUserRepository extends JpaRepository< User, Long >
 {
-
+    @Query
+    (
+        value = "SELECT u.* FROM user u WHERE u.id IN(?1)",
+        nativeQuery = true
+    )
+    Set<User> searchUsersById( Set<Long> ids );
 }
