99c8571621ecf0b58263774aba48c4efc7618f19
Nicolas <nicolas.echenique.117@gmail.com>
2024-04-12 18:02:35 -0300

    Optimizando metodos de los services user y grupo

--- src/main/java/com/registro/grupos/demo/service/GrupoService.java
+++ src/main/java/com/registro/grupos/demo/service/GrupoService.java
@@ -1,11 +1,14 @@
 package com.registro.grupos.demo.service;
 
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.registro.grupos.demo.dto.GrupoDTO;
 import com.registro.grupos.demo.model.Grupo;
+import com.registro.grupos.demo.model.User;
 import com.registro.grupos.demo.repository.IGrupoRepository;
 
 import jakarta.transaction.Transactional;
@@ -17,9 +20,21 @@ public class GrupoService
     @Autowired
     private IGrupoRepository iGrupoRepository;
 
-    public Grupo findOne (Long id)
+    public Grupo findGrupoById ( Long id )
     {
         return iGrupoRepository.findById(id).orElseThrow();
+    }
+
+    public GrupoDTO findOne (Long id)
+    {
+        Grupo grupo = iGrupoRepository.findById(id).orElseThrow();
+
+        GrupoDTO grupoDTO = new GrupoDTO();
+        grupoDTO.setId( grupo.getId() );
+        grupoDTO.setName( grupo.getName() );
+        grupoDTO.setLogo( grupo.getLogo() );
+
+        return grupoDTO;
     }
 
     public List<Grupo> findAll()
@@ -47,4 +62,18 @@ public class GrupoService
         return iGrupoRepository.existsById(id);
     }
     
+    public Set<Grupo> searchGruposById ( Set<Long> ids )
+    {
+        Set<Grupo> grupos = iGrupoRepository.searchGruposById(ids);
+
+        return grupos;
+    }
+
+    public GrupoDTO loadGrupo ( GrupoDTO grupoDTO, Set<User> usersList )
+    {
+        GrupoDTO grupo = grupoDTO;
+        grupo.setUsers( usersList );
+
+        return grupo;
+    }
 }
--- src/main/java/com/registro/grupos/demo/service/UserService.java
+++ src/main/java/com/registro/grupos/demo/service/UserService.java
@@ -1,10 +1,13 @@
 package com.registro.grupos.demo.service;
 
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.registro.grupos.demo.dto.UserDTO;
+import com.registro.grupos.demo.model.Grupo;
 import com.registro.grupos.demo.model.User;
 import com.registro.grupos.demo.repository.IUserRepository;
 
@@ -17,9 +20,23 @@ public class UserService
     @Autowired
     private IUserRepository iUserRepository;
 
-    public User findOne (Long id)
+    public User findUserbyId ( Long id )
     {
-        return iUserRepository.findById(id).orElseThrow();
+        User user = iUserRepository.findById(id).orElseThrow();
+
+        return user;
+    }
+
+    public UserDTO findOne (Long id)
+    {
+        User user = iUserRepository.findById(id).orElseThrow();
+
+        UserDTO userDTO = new UserDTO();
+        userDTO.setId( user.getId() );
+        userDTO.setAvatar( user.getAvatar() );
+        userDTO.setName( user.getName() );
+
+        return userDTO;
     }
 
     public List<User> findAll()
@@ -47,4 +64,18 @@ public class UserService
         return iUserRepository.existsById(id);
     }
 
+    public Set<User> searchUsersByIds ( Set<Long> ids )
+    {
+        Set<User> users = iUserRepository.searchUsersById(ids);
+
+        return users;
+    }
+
+    public UserDTO loadUser ( UserDTO userDTO, Set<Grupo> grupos )
+    {
+        UserDTO user = userDTO;
+        user.setGrupos(grupos);
+
+        return user;
+    }
 }
