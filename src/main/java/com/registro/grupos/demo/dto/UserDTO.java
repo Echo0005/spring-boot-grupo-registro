fde7fd45a20427b68cb649193ef1fd57d3d9df26
Nicolas <nicolas.echenique.117@gmail.com>
2024-04-12 17:59:21 -0300

    Agregando hashset a los dto

--- src/main/java/com/registro/grupos/demo/dto/UserDTO.java
+++ src/main/java/com/registro/grupos/demo/dto/UserDTO.java
@@ -1,5 +1,9 @@
 package com.registro.grupos.demo.dto;
 
+import java.util.Set;
+
+import com.registro.grupos.demo.model.Grupo;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,4 +16,6 @@ public class UserDTO
     private Long id;
     private String name;
     private String avatar;
+
+    private Set<Grupo> grupos;
 }
